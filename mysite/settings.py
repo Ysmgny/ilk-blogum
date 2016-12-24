9969ec327b7ad5ec5d4441f414094e2215311f35
ysmgny <yesimselcen95@hotmail.com>
2016-12-24 17:34:47 +0300

    debug false oldu

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pb%l(!2y(jg&ss=(cf=fyw4b)p$fhm&x40%hfobq(fn9@gv8m$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
